ee231541e4dd5e858c1a5d095aae06c424a206bf
Pilot338 <46417154+Pilot-Khadka@users.noreply.github.com>
2022-05-15 07:41:20 +0545

    Update main.py
    Added decrypt function

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ def text_to_ascii(data):
         data_ascii.append(index)
     return data_ascii
 
-
 def remove_space(data):
-    spaces = [pos for pos, char in enumerate(data) if char == ' ']
-    data.replace(' ','')
+    spaces = [pos for pos, char in enumerate(data) if char == " "]
+    data.replace(" ","")
     return data,spaces
-    
     
 def add_space(data,spaces):
     for space in spaces:
@@ -54,7 +52,38 @@ def encrypt(data):
     return cipher
 
 
+# decrypting function
+def decrypt(data):
+    data,space = remove_space(data)
+    
+    text = text_to_ascii(data)
+    temp = 0
+    cipher = ''
+    
+    for letter in text:
+        if letter in range(128):
+            if letter == 32:
+                cipher += chr(letter)
+            elif letter < 64:
+                index = letter - ord('0')
+                temp = (index - SHIFT) % 10
+                cipher += chr(temp + ord('0'))
+            elif letter < 97:
+                index = letter - ord('A')
+                temp = (index - SHIFT) % 26
+                cipher += chr(temp + ord('A'))
+            else:
+                index = letter - ord('a')
+                temp = (index - SHIFT) % 26
+                cipher += chr(temp + ord('a'))
+                
+    add_space(cipher,space)
+    return cipher
+
 if __name__ == '__main__':
     usr_data = input("Enter the text to Encrypt:")
     encrypted = encrypt(usr_data)
     print(encrypted)
+    
+    decrypted = decrypt(encrypted)
+    print(decrypted)
