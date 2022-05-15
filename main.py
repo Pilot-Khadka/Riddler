c3d5c390b3dd21a0fe4efb90cfaabec8dd2cac5e
Amit Pant <50907565+amitpant7@users.noreply.github.com>
2022-05-15 16:24:39 +0545

    Resolved conflicts

--- main.py
+++ main.py
@@ -12,55 +12,29 @@ def text_to_ascii(data):
         data_ascii.append(index)
     return data_ascii
 
-def remove_space(data):
-    spaces = [pos for pos, char in enumerate(data) if char == " "]
-    data.replace(" ","")
-    return data,spaces
-    
-def add_space(data,spaces):
-    for space in spaces:
-        data = data[:space] + ' ' + data[space:]
-    return data
-    
-    
-# encrypting and decrypting fuction
-def cipher(mode,data):
-    data,space = remove_space(data)
-    
+
+# encrypting fuction
+def encrypt(data):
     text = text_to_ascii(data)
     temp = 0
     cipher = ''
-    
     for letter in text:
         if letter in range(128):
             if letter == 32:
                 cipher += chr(letter)
-                
             elif letter < 64:
                 index = letter - ord('0')
-                if mode == 'encrypt':
-                    temp = (index + SHIFT) % 10
-                elif mode == 'decrypt':
-                    temp = (index - SHIFT) % 10
+                temp = (index + SHIFT) % 10
                 cipher += chr(temp + ord('0'))
-                
             elif letter < 97:
                 index = letter - ord('A')
-                if mode == 'encrypt':
-                    temp = (index + SHIFT) % 26
-                elif mode == 'decrypt':
-                    temp = (index - SHIFT) % 26
+                temp = (index + SHIFT) % 26
                 cipher += chr(temp + ord('A'))
-                
             else:
                 index = letter - ord('a')
-                if mode == 'encrypt':
-                    temp = (index + SHIFT) % 26
-                elif mode == 'decrypt':
-                    temp = (index - SHIFT) % 26
+                temp = (index + SHIFT) % 26
                 cipher += chr(temp + ord('a'))
-                
-    add_space(cipher,space)
+
     return cipher
 
 
